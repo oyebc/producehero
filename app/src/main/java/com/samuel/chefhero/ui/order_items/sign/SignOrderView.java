74c429e249953fb2b51a53a0bdd3b29bfade0152
Samuel Oyediran <samueloyebisi108@gmail.com>
2019-10-17 13:53:07 -0400

    push login page back to the fore.

---
+++ app/src/main/java/com/samuel/chefhero/ui/order_items/sign/SignOrderView.java
@@ -0,0 +1,13 @@
+package com.samuel.chefhero.ui.order_items.sign;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.view.View;
+
+public class SignOrderView extends View {
+
+    private Paint signPaint;
+    public SignOrderView(Context context) {
+        super(context);
+    }
+}
